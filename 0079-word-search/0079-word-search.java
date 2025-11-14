7a4962217da66ec6e3b0ab69bf714c9ec1f0c18c
Anand Manuvanjay Awasthi <166633429+Anand-Manuvanjay-Awasthi@users.noreply.github.com>
2025-11-15 00:59:24 +0530

    Time: 144 ms (51.66%), Space: 42.9 MB (28.86%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -11,20 +11,20 @@ class Solution {
             for (int j = 0; j < board[i].length; j++) {
 
                 if (board[i][j] == word.charAt(0)) {
-                    if(existhelp(board,word,"",i,j,visited)) return true;
+                    if(existhelp(board,word,0,i,j,visited)) return true;
                 }
             }
         }
         return false;
     }
-    private boolean existhelp(char[][] board, String word,String ans,int i ,int j,boolean[][] visited){
-        if(ans.length()==word.length()) return true;
+    private boolean existhelp(char[][] board, String word,int ind,int i ,int j,boolean[][] visited){
+        if(ind==word.length()) return true;
         if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) return false;
-        if (board[i][j] != word.charAt(ans.length())) return false;
+        if (board[i][j] != word.charAt(ind)) return false;
         if(visited[i][j]) return false;
         visited[i][j] = true;
-        if(existhelp(board,word,ans+board[i][j],i,j-1,visited) || existhelp(board,word,ans+board[i][j],i,j+1,visited)
-        ||existhelp(board,word,ans+board[i][j],i+1,j,visited)||existhelp(board,word,ans+board[i][j],i-1,j,visited)) return true;
+        if(existhelp(board,word,ind+1,i,j-1,visited) || existhelp(board,word,ind+1,i,j+1,visited)
+        ||existhelp(board,word,ind+1,i+1,j,visited)||existhelp(board,word,ind+1,i-1,j,visited)) return true;
 
         visited[i][j] = false;
         return false;
