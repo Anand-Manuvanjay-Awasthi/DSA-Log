b8a30099eeb602023eb9e95e4e4e4ddb6f9f73ef
Anand Manuvanjay Awasthi <166633429+Anand-Manuvanjay-Awasthi@users.noreply.github.com>
2025-11-28 14:33:25 +0530

    Time: 34 ms (5.05%), Space: 96.1 MB (9.46%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer,Integer> mp=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+           mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+        }
+        for(int i:mp.values()){
+            if(i>1) return true;
+        }
+        return false;
+    }
+}
